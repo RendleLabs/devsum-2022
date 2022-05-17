c37b99f19ac0ff00abd7679a10189355d7748194
Mark Rendle <mark@rendlelabs.com>
2022-05-17 19:11:04 +0200

    Initial commit

---
+++ src/Ingredients.Data/Constants.cs
@@ -0,0 +1,6 @@
+namespace Ingredients.Data;
+
+public static class Constants
+{
+    public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=devsum;AccountKey=5soU/UXOaxIfDYtEtLReR2AE1NrLfe1eAKP2LTHDfPxmQi2gdcf6JNJ0xD83WPTkU3kLaiq4rLYwDg69NceaGQ==;EndpointSuffix=core.windows.net";
+}
