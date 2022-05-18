5f9b07772112d8f586e7c56f9d4efed7c8032074
Mark Rendle <mark@rendlelabs.com>
2022-05-18 13:27:08 +0200

    Mock IToppingData

--- test/Ingredients.Tests/IngredientsServiceTests.cs
+++ test/Ingredients.Tests/IngredientsServiceTests.cs
@@ -1,6 +1,11 @@
 using Grpc.Net.Client;
+using Ingredients.Data;
 using Ingredients.Tests.Protos;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NSubstitute;
 
 namespace Ingredients.Tests;
 
@@ -20,7 +25,15 @@ public class IngredientsServiceTests : IClassFixture<IngredientsApplicationFacto
 
         var response = await client.GetToppingsAsync(new GetToppingsRequest());
 
-        Assert.NotEmpty(response.Toppings);
+        Assert.Collection(response.Toppings,
+            t =>
+            {
+                Assert.Equal("cheese", t.Id);
+            },
+            t =>
+            {
+                Assert.Equal("tomato", t.Id);
+            });
     }
 }
 
@@ -36,5 +49,30 @@ public class IngredientsApplicationFactory
                 HttpClient = httpClient
             });
         return new IngredientsService.IngredientsServiceClient(channel);
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IToppingData>();
+            services.AddSingleton(SubToppingData());
+        });
+    }
+
+    private static IToppingData SubToppingData()
+    {
+        var toppings = new List<ToppingEntity>
+        {
+            new("cheese", "Cheese", 0.5d, 10),
+            new("tomato", "Tomato", 0.5d, 10),
+        };
+
+        var sub = Substitute.For<IToppingData>();
+
+        sub.GetAsync(Arg.Any<CancellationToken>())
+            .Returns(toppings);
+
+        return sub;
     }
 }
